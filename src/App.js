170fbbf14b200fc31da424e2f6463ff258b60152
anjalyyy1 <anjalyyy1@gmail.com>
2019-06-02 21:02:18 +0530

    tabs added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Table from './components/reactTable';
 import FilterTable from './components/FilterTable';
+import Tab1 from './components/Tab1';
+import Tab2 from './components/Tab2';
+import Tab3 from './components/Tab3';
 
+import Tabs from './components/Tabs';
 
 class App extends Component {
   render() {
@@ -11,6 +15,21 @@ class App extends Component {
       <div className="App">
         {/* <Table/> */}
         <FilterTable/>
+
+        <div>
+          Tabs demo
+          <Tabs>
+            <div label="Gator">
+             <Tab1/>
+            </div>
+            <div label="Croc">
+            <Table/>
+            </div>
+            <div label="Sarcosuchus">
+              <Tab3/>
+            </div>
+        </Tabs>
+        </div>
       </div>
     );
   }
---
+++ src/components/Tab2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tab2 = () => {
+  return (
+    <div>tab2</div>
+  )
+}
+
+export default Tab2;
